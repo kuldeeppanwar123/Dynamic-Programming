a8df830123f2c3485b9969c883a2195e469852a1
Kuldeep Panwar <91942775+kuldeeppanwar123@users.noreply.github.com>
2023-11-07 16:14:58 +0530

    Update 16_partition_equal_subset_sum.cpp

--- 16_partition_equal_subset_sum.cpp
+++ 16_partition_equal_subset_sum.cpp
@@ -63,9 +63,42 @@ bool subsetSumToK(int n, int k, vector<int> &arr) {
 ---------------------------------------------------------------------------------
 
 TABULATION:
-    time : O()
-    space : O()
+      time : O(N*target)
+      space : O(N*target)
     code : 
+
+    bool canPartition(vector<int>& arr) {
+    
+        int sum = 0;
+        for(int itm:arr){
+            sum+=itm;
+        }
+        if(sum%2!=0)return false;
+        int n = arr.size();
+        int k = sum/2;
+
+        vector<vector<int>>dp(n,vector<int>(k+1,0));
+  
+        for(int idx=0; idx<n; idx++){
+            dp[idx][0] =1;
+        }
+
+        if(arr[0]<=k)dp[0][arr[0]] = 1;
+
+        for(int idx=1; idx<n; idx++){
+            for(int target=1; target<=k; target++){
+                bool notTake = dp[idx-1][target];
+                bool take = false;
+                if(arr[idx]<=target)
+                take = dp[idx-1][target-arr[idx]];
+
+                dp[idx][target] = notTake || take;
+            }
+        }
+
+        return dp[n-1][k];
+        
+    }
 
 --------------------------------------------------------------------------------------------------------
 
